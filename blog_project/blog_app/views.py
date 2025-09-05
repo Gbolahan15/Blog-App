677cce62cc40915f6174cf2bce32d267f036d5ec
Gbolahan <adesinagbolahan15@gmail.com>
2025-09-05 21:24:02 +0100

    created the homepage and category views function

--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,16 +1,21 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Category, Post, Comment
 
 # Create your views here.
 
 def homepage_view(request):
-    pass
+    posts = Post.objects.filter(published=True).order_by('-created_at')
+    return render(request, 'home.html', {'posts':posts})
 
-def category_view(request):
-    pass
+def category_view(request, category_name):
+    category = get_object_or_404(Category, name=category_name)
+    posts = Post.objects.filter(category=category, published=True).order_by('-created_at')
+    return render(request, 'category.html', {'category':category, 'posts':posts})
 
-def post_view(request):
-    pass
+def post_view(request, slug):
+    post = get_object_or_404(Post, slug=slug, published=True)
+    comments = post.comments.filter(approved=True)
+    return render(request, 'post_detail.html', {'post':post, 'comments':comments})
 
-def comment_view(request):
+def comment_view(request, slug):
     pass
