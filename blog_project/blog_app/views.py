8c8b5d559c3017bdac787717ab40abed7d96e37f
Gbolahan <adesinagbolahan15@gmail.com>
2025-09-05 17:29:34 +0100

    creating the functions for the views

--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from .models import Category, Post, Comment
 
 # Create your views here.
+
+def homepage_view(request):
+    pass
+
+def category_view(request):
+    pass
+
+def post_view(request):
+    pass
+
+def comment_view(request):
+    pass
