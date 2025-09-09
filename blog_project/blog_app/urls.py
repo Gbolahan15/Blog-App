b382e8bbe15f1e8a16ee68ce525096bc26164e82
Gbolahan <adesinagbolahan15@gmail.com>
2025-09-09 21:34:52 +0100

    added the category template and forgot a '/' in the urls

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.homepage_view, name="home"),
     path('post/<slug:slug>/', views.post_view, name="post_detail"),
-    path('category/<str:category_name>', views.category_view, name="category"),
+    path('category/<str:category_name>/', views.category_view, name="category"),
     path('post/<slug:slug>/comment/', views.comment_view, name="comment"),
 ]
