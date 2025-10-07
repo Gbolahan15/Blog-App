8d3760bce019ff470f2af83eca1bb05b303ae553
Gbolahan <adesinagbolahan15@gmail.com>
2025-10-07 21:24:52 +0100

    added to the urls.py

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'categories', views.CategoryViewSet)
+router.register(r'posts', views.PostViewSet)
+router.register(r'comments', views.CommentViewSet)
 
 urlpatterns = [
     path('', views.homepage_view, name="home"),
     path('post/<slug:slug>/', views.post_view, name="post_detail"),
     path('category/<str:category_name>/', views.category_view, name="category"),
-    # path('post/<slug:slug>/comment/', views.comment_view, name="comment"),
+    path('api/', include(router.urls)),
 ]
